69d5e2ba9f14d05052d0f50911081f5b36f0cff7
Jayanga Dissanayake <jsdjayanga@gmail.com>
2018-04-18 22:02:42 +0530

    Fix linux-deb script

--- installers/linux-deb/build-ballerina-linux-deb-x64.sh
+++ installers/linux-deb/build-ballerina-linux-deb-x64.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-function pringUsage() {
+function printUsage() {
     echo "Usage:"
-    echo "build.sh [options]"
+    echo "$0 [options]"
     echo "options:"
     echo "    -v (--version)"
-    echo "        version of the balletina distribution"
+    echo "        version of the ballerina distribution"
     echo "    -p (--path)"
     echo "        path of the ballerina distributions"
     echo "    -d (--dist)"
-    echo "        balletina distribution type either of the followings"
+    echo "        ballerina distribution type either of the followings"
     echo "        If not specified both distributions will be built"
     echo "        1. ballerina-platform"
     echo "        2. ballerina-runtime"
-    echo "eg: build.sh -v 1.0.0 -p /home/username/Packs"
-    echo "eg: build.sh -v 1.0.0 -d ballerina-platform"
+    echo "eg: $0 -v 1.0.0 -p /home/username/Packs"
+    echo "eg: $0 -v 1.0.0 -p /home/username/Packs -d ballerina-platform"
 }
 
 BUILD_ALL_DISTRIBUTIONS=false
@@ -23,7 +23,7 @@ while [[ $# -gt 0 ]]
 do
 key="$1"
 
-case $key in
+case ${key} in
     -v|--version)
     BALLERINA_VERSION="$2"
     shift # past argument
@@ -46,13 +46,6 @@ case $key in
 esac
 done
 
-#for i in "${POSITIONAL[@]}"
-#do
-#    if [ "$i" == "--all" ]; then
-#        BUILD_ALL_DISTRIBUTIONS=true
-#    fi
-#done
-
 if [ -z "$BALLERINA_VERSION" ]; then
     echo "Please enter the version of the ballerina pack"
     pringUsage
@@ -67,44 +60,14 @@ fi
 
 if [ -z "$DISTRIBUTION" ]; then
     BUILD_ALL_DISTRIBUTIONS=true
-    #echo "You have to use either --all or -d [distribution]"
-    #pringUsage
-    #exit 1
 fi
 
+BALLERINA_DISTRIBUTION_LOCATION=${DIST_PATH}
+BALLERINA_PLATFORM=ballerina-platform-linux-${BALLERINA_VERSION}
+BALLERINA_RUNTIME=ballerina-runtime-linux-${BALLERINA_VERSION}
+BALLERINA_INSTALL_DIRECTORY=ballerina-${BALLERINA_VERSION}
 
-
-
-BALLERINA_DISTRIBUTION_LOCATION=$DIST_PATH
-BALLERINA_PLATFORM=ballerina-platform-linux-$BALLERINA_VERSION
-BALLERINA_RUNTIME=ballerina-runtime-linux-$BALLERINA_VERSION
-BALLERINA_INSTALL_DIRECTORY=ballerina-$BALLERINA_VERSION
-#BALLERINA_PLATFORM_ZIP=$BALLERINA_DISTRIBUTION_LOCATION/$DISTRIBUTION-$BALLERINA_VERSION.zip
-
-#BALLERINA_VERSION=0.970.0-alpha1-SNAPSHOT
-#BALZIP=/home/ubuntu/Packs/ballerina-linux-$BALLERINA_VERSION.zip
-#BALDIST=ballerina-linux-$BALLERINA_VERSION
-
-echo $BALDIST "build started at" $(date +"%Y-%m-%d %H:%M:%S")
-
-#rm -rf target
-#mkdir -p target/original
-#unzip $BALZIP -d target/original > /dev/null 2>&1
-#mv target/original/$BALDIST target/original/ballerina-linux-$BALLERINA_VERSION
-
-#mkdir -p target/$BALDIST/opt/ballerina
-#chmod -R 755 target/$BALDIST/opt/ballerina
-
-###mv target/original/ballerina-linux-$BALLERINA_VERSION target/$BALDIST/opt/ballerina 
-
-#cp -R linux/DEBIAN target/$BALDIST/DEBIAN
-#sed -i -e 's/__BALLERINA_VERSION__/'$BALLERINA_VERSION'/g' target/$BALDIST/DEBIAN/postinst
-#sed -i -e 's/__BALLERINA_VERSION__/'$BALLERINA_VERSION'/g' target/$BALDIST/DEBIAN/postrm
-#sed -i -e 's/__BALLERINA_VERSION__/'$BALLERINA_VERSION'/g' target/$BALDIST/DEBIAN/control
-
-#dpkg-deb --build target/$BALDIST
-
-echo $BALDIST "build completed at" $(date +"%Y-%m-%d %H:%M:%S")
+echo "Build started at" $(date +"%Y-%m-%d %H:%M:%S")
 
 function deleteTargetDirectory() {
     echo "Deleting target directory"
@@ -116,41 +79,41 @@ function extractPack() {
     rm -rf target/original
     mkdir -p target/original
     unzip $1 -d target/original > /dev/null 2>&1
-    mv target/original/$2 target/original/$BALLERINA_INSTALL_DIRECTORY
+    mv target/original/$2 target/original/${BALLERINA_INSTALL_DIRECTORY}
 }
 
 function createPackInstallationDirectory() {
-    rm -rf target/$BALLERINA_INSTALL_DIRECTORY/opt/ballerina
-    mkdir -p target/$BALLERINA_INSTALL_DIRECTORY/opt/ballerina
-    chmod -R 755 target/$BALLERINA_INSTALL_DIRECTORY/opt/ballerina
-    mv target/original/$BALLERINA_INSTALL_DIRECTORY target/$BALLERINA_INSTALL_DIRECTORY/opt/ballerina
+    rm -rf target/${BALLERINA_INSTALL_DIRECTORY}/opt/ballerina
+    mkdir -p target/${BALLERINA_INSTALL_DIRECTORY}/opt/ballerina
+    chmod -R 755 target/${BALLERINA_INSTALL_DIRECTORY}/opt/ballerina
+    mv target/original/${BALLERINA_INSTALL_DIRECTORY} target/${BALLERINA_INSTALL_DIRECTORY}/opt/ballerina
 }
 
 function copyDebianDirectory() {
-    cp -R resources/DEBIAN target/$BALLERINA_INSTALL_DIRECTORY/DEBIAN
-    sed -i -e 's/__BALLERINA_VERSION__/'$BALLERINA_VERSION'/g' target/$BALLERINA_INSTALL_DIRECTORY/DEBIAN/postinst
-    sed -i -e 's/__BALLERINA_VERSION__/'$BALLERINA_VERSION'/g' target/$BALLERINA_INSTALL_DIRECTORY/DEBIAN/postrm
-    sed -i -e 's/__BALLERINA_VERSION__/'$BALLERINA_VERSION'/g' target/$BALLERINA_INSTALL_DIRECTORY/DEBIAN/control
+    cp -R resources/DEBIAN target/${BALLERINA_INSTALL_DIRECTORY}/DEBIAN
+    sed -i -e 's/__BALLERINA_VERSION__/'${BALLERINA_VERSION}'/g' target/${BALLERINA_INSTALL_DIRECTORY}/DEBIAN/postinst
+    sed -i -e 's/__BALLERINA_VERSION__/'${BALLERINA_VERSION}'/g' target/${BALLERINA_INSTALL_DIRECTORY}/DEBIAN/postrm
+    sed -i -e 's/__BALLERINA_VERSION__/'${BALLERINA_VERSION}'/g' target/${BALLERINA_INSTALL_DIRECTORY}/DEBIAN/control
 }
 
 function createBallerinaPlatform() {
     echo "Creating ballerina platform installer"
-    #BALLERINA_INSTALL_DIRECTORY=$BALLERINA_PLATFORM
-    extractPack "$BALLERINA_DISTRIBUTION_LOCATION/$BALLERINA_PLATFORM.zip" $BALLERINA_PLATFORM
+
+    extractPack "$BALLERINA_DISTRIBUTION_LOCATION/$BALLERINA_PLATFORM.zip" ${BALLERINA_PLATFORM}
     createPackInstallationDirectory
     copyDebianDirectory
-    mv target/$BALLERINA_INSTALL_DIRECTORY target/ballerina-platform-linux-installer-x64-$BALLERINA_VERSION
-    dpkg-deb --build target/ballerina-platform-linux-installer-x64-$BALLERINA_VERSION
+    mv target/${BALLERINA_INSTALL_DIRECTORY} target/ballerina-platform-linux-installer-x64-${BALLERINA_VERSION}
+    dpkg-deb --build target/ballerina-platform-linux-installer-x64-${BALLERINA_VERSION}
 }
 
 function createBallerinaRuntime() {
     echo "Creating ballerina runtime installer"
-    #BALLERINA_INSTALL_DIRECTORY=$BALLERINA_RUNTIME
-    extractPack "$BALLERINA_DISTRIBUTION_LOCATION/$BALLERINA_RUNTIME.zip" $BALLERINA_RUNTIME
+
+    extractPack "$BALLERINA_DISTRIBUTION_LOCATION/$BALLERINA_RUNTIME.zip" ${BALLERINA_RUNTIME}
     createPackInstallationDirectory
     copyDebianDirectory
-    mv target/$BALLERINA_INSTALL_DIRECTORY target/ballerina-runtime-linux-installer-x64-$BALLERINA_VERSION
-    dpkg-deb --build target/ballerina-runtime-linux-installer-x64-$BALLERINA_VERSION
+    mv target/${BALLERINA_INSTALL_DIRECTORY} target/ballerina-runtime-linux-installer-x64-${BALLERINA_VERSION}
+    dpkg-deb --build target/ballerina-runtime-linux-installer-x64-${BALLERINA_VERSION}
 }
 
 deleteTargetDirectory
@@ -170,3 +133,5 @@ else
         echo "Error"
     fi
 fi
+
+echo "Build completed at" $(date +"%Y-%m-%d %H:%M:%S")
